14091aaf3f1f6f61e1d787a4f776cd94129beb12
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2013-05-23 12:27:53 -0700

    php example really added

---
+++ examples/php/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8"/>
+    <title>
+      Example for Berkeley Ecoinformatics Engine
+    </title>
+  </head>
+
+  <body>
+    <ul>
+
+    <?php
+
+    $url = 'http://ecoengine.berkeley.edu/api/photos/?format=json&page_size=30&collection_code=VTM';
+    $json = file_get_contents($url);
+    # use php notation if using json_decode
+    # however the $json content can be parsed in different ways (it is a simple text on load)
+    $obj = json_decode($json);
+
+    foreach ($obj -> features as $feature) {
+        $line = '<li><a href="http://"'.$feature -> properties -> media_url.'">';
+        $line .= $feature -> properties -> record.'</li>';
+        echo $line;
+    }
+
+    ?>
+
+    </ul>
+</html>
