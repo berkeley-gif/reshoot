2870435ab6fd055edd50178e32ba7a2ff7041e2b
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2013-05-22 17:47:18 -0700

    workin example

--- examples/js/example.js
+++ examples/js/example.js
@@ -1,8 +1,25 @@
 var bee_example = {
+
   'init': function(){
-    alert('test');
+    //var url = 'http://localhost:8000/api/photos/?format=json';
+    var url = 'http://ecoengine.berkeley.edu/api/photos/?format=json';
+
+    $.ajax({
+      dataType: "json",
+      contentType: "application/json",
+      url: url,
+      data: 'authors=Wieslander&page_size=30',
+      success: function(data){
+        $.each(data.features, function(i, f) {
+          line = ''.concat('<li><a href="http:///', f.properties.media_url, '">', f.properties.record, '</a></li>')
+          $('ul').append(line)
+        });
+      }
+    });
+
   }
 }
+
 
 $(document).ready(
   function(){
